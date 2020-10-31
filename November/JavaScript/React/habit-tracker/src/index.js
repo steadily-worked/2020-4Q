8825c5d94909d4f921fb2472e8e9078672f7a777
sangminpark <qpwpep5429@gmail.com>
2020-10-31 14:44:19 +0900

    10/31 React

--- November/JavaScript/React/habit-tracker/src/index.js
+++ November/JavaScript/React/habit-tracker/src/index.js
@@ -4,8 +4,8 @@ import './index.css';
 import App from './app';
 
 ReactDOM.render(
-  <React.StrictMode>,
-  <App />,
+  <React.StrictMode>
+    <App />
   </React.StrictMode>,
-  document.getElementById('root') // 이 뒤에 위 행의 root component를 연결.
+  document.getElementById('root')
 );
