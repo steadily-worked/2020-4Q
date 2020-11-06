2af4745140bd98dbac3e0663e7238b4ba8201c63
sangminpark <qpwpep5429@gmail.com>
2020-11-06 11:16:36 +0900

    11/5 React ~delete

--- November/JavaScript/React/habit-tracker/src/components/habit.jsx
+++ November/JavaScript/React/habit-tracker/src/components/habit.jsx
@@ -1,31 +1,52 @@
 import React, { Component } from 'react';
 
 class Habit extends Component {
-    state = {
-        count: 0,
+    // state = {
+    //     count: 0,
+    // };
+    // habit이 추가되고 난 후엔 state가 필요가 없게 된다.
+    handleIncrement = () => {
+        // // state 오브젝트 안에 있는 count를 증가한 뒤 state를 업데이트한다.
+        // this.setState({ count: this.state.count + 1 });
+        // // 전체적인 state에 대한 명령을 줘야 React가 알아차린다!
+        // // this.state.count ++; 라든지 this.state.count += 1 이런식으로 쓰면 안된다는 뜻임!
+        // 위 4줄은, habit을 넣기 전 코드이다.
+        this.props.onIncrement(this.props.habit);
+    };
+    handleDecrement = () => {
+        // const count = this.state.count - 1;
+        // this.setState({ count: count < 0 ? 0 : count })
+        // 위 2줄은, habit을 넣기 전 코드이다.
+        this.props.onDecrement(this.props.habit);
     };
 
-    handleIncrement = () => {
-        // state 오브젝트 안에 있는 count를 증가한 뒤 state를 업데이트한다.
-        this.setState({ count: this.state.count + 1 });
-    }
-    handleDecrement = () => {
-        const count = this.state.count - 1;
-        this.setState({ count: count < 0 ? 0 : count })
-    }
+    handleDelete = () => {
+        this.props.onDelete(this.props.habit);
+    };
+
     render() {
         const { name, count } = this.props.habit;
         return (
         <li className='habit'>
         <span className="habit-name">{name}</span>
         <span className="habit-count">{count}</span>
-        <button className='habit-button habit-increase' onClick={this.handleIncrement}>
+        <button
+        className='habit-button habit-increase'
+        onClick={this.handleIncrement}>
             <i className="fas fa-plus-square"></i>
         </button>
-        <button className='habit-button habit-decrease' onClick={this.handleDecrement}>
+        <button
+        className='habit-button habit-decrease'
+        onClick={this.handleDecrement}>
             <i className="fas fa-minus-square"></i>
         </button>
-        <button className='habit-button habit-trash'>
+        <button
+        className='habit-button habit-delete'
+        // onClick={() => {
+        //     this.props.onDelete(this.props.habit);
+        //     }} // 해당 function이 계속 실행 되는 것을 막고싶다면
+        onClick={this.handleDelete} // this.handleDelete로 연결해준 후 handleDecrement() 아래에 handleDelete()를 선언해주면 된다.
+        >
             <i className="fas fa-trash"></i>
         </button>
         </li>
@@ -34,3 +55,14 @@ class Habit extends Component {
 }
 
 export default Habit;
+
+//19행 추가 이후에 +- 기능이 제대로 되지 않는 이유는, 우리가 habit에서 props를 전달해서 우리가 props를 표기하게 되는데,
+// (19행을 통해) 여기에서 증가한, 내부적으로 쓰이는 State는 더이상 쓰이고 있지 않게 된다. 그래서 안되는 것.
+// data가 habits.jsx의 state 내에 있는 habits에 리스트 형태로 존재하고 있기 때문에 그것을 가능케 하는 로직 또한
+// state 내에 있어야 한다.
+
+
+// 작은 컴포넌트 안에 자체적으로 state가 들어있는 경우가 있는데, 만약 이state가 공통적으로 사용되는 부분이 있다면
+// state를 상위 컴포넌트로 갖고 나오고 부모 컴포넌트에서 해당하는 데이터를 props로 전달해주면 된다. 그리고 데이터 업뎃을 위해선
+// prop으로 전달된 callback 함수를 이용해서 실제로 데이터를 갖고 있는 부모 component에서 처리가 되도록 만든 것임. 이게 state와 Props의 차이
+// state는 컴포넌트가 자체적으로 갖고 있는 데이터이고, props는 외부에서 전달받은 데이터이다. 
--- November/JavaScript/React/habit-tracker/src/components/habits.jsx
+++ November/JavaScript/React/habit-tracker/src/components/habits.jsx
@@ -9,11 +9,44 @@ class Habits extends Component {
             { id: 3, name: 'Coding', count: 0 },
         ],
     };
+    handleIncrement = habit => {
+        // this.state.habits[index].count++; 가 안되는 이유는, 이렇게 부분적으로 변경해주면 모르기 때문..
+        const habits = [...this.state.habits]; // state에 있는 habits의 오브젝트를 그대로 복사해서 habits const로 할당(새로운 배열)
+        const index = habits.indexOf(habit); // 배열에서 몇 번째에 있는지 물어보는 const
+        habits[index].count++; // index번째에 있는 habits의 카운트를 1 증가
+        // 그러니까 .. 14행을 통해 habits의 오브젝트를 그대로 가져온다. (그 이유는, 부분적으로만 변경하면 React가 이를 인지하지 못한다. 그래서 전체적으로 state를 업뎃해야함.)
+        // 그리고 indexOf(habit)으로 habit의 위치를 파악하는 index 상수를 만든다.
+        // 그 이후에, habits 내의 index번째의 카운트를 1 증가시킨다.
+        // habits가 10개 정도가 있다고 할 때, 첫 번째를 증가시키는 버튼을 한 번 누를 경우 index[0]의 count가 1이 증가되는 것.
+        this.setState({ habits });
+        // 바꿔주고 난 후 setState를 통해 habits 전체를 업데이트 해줘야 React에서 인식한다. 부분적으로 변경하면 React는 모른다.
+    };
+    handleDecrement = habit => {
+        const habits = [...this.state.habits];
+        const index = habits.indexOf(habit);
+        const count = habits[index].count - 1;
+        habits[index].count = count < 0 ? 0 : count; // count - 한 값이 0보다 작으면 0을 쓰고 아니면 계산한 새로운 count를 할당
+        this.setState({ habits });
+    };
+    
+    handleDelete = habit => {
+        const habits = this.state.habits.filter(item => item.id !== habit.id); // 새로운 배열을 만드는데, this.state.habits을 빙글빙글 돌면서, (habit이 전달되면(item) item의 id와 habit의 id가 다른 경우만 가져오기)
+        this.setState({ habits }); // 전달된 habit의 id가 아닌 것들만(즉 32행의 habit만) 빼놓고 복사해서 habits로 지정 후 그것만 state를 habit으로 지정.
+
+    };
+
     render() {
         return (
           <ul>
             {this.state.habits.map(habit => (
-                <Habit key={habit.id} habit={habit} />
+                // <Habit key={habit.id} habit={habit} /> // habit이라는(등호왼쪽) property 안에 각각의 {Habit}을 전달.
+                // 윗줄은 habit을 넣기 전 코드임.
+                <Habit
+                key={habit.id}
+                habit={habit}
+                onIncrement={this.handleIncrement}
+                onDecrement={this.handleDecrement}
+                onDelete={this.handleDelete} />
             ))}
           </ul>
         );
@@ -21,3 +54,4 @@ class Habits extends Component {
 }
 
 export default Habits;
+
